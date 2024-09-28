3245435ea8f2ace7086368cdd04f0c931a354196
LissetteRedrovan <lj.redrovan@uea.edu.ec>
2024-09-28 11:03:36 -0500

    VALORES DICCIONARIO

---
+++ SEMANA15/TAREA_EN_CLASE_DICCIONARIO.py
@@ -0,0 +1,11 @@
+#listado
+libros = {
+    "nombre_del_libro": "Patito feo",
+    "categoria": "Cuento",
+    "año_de_publicacion": 1843,
+    "numero_de_hojas": 44,
+    "autor": " Hans Christian Anderse"
+
+}
+
+print(libros)
